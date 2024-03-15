62555f1be0e058b69b49ce06fa58984ee21f4666
William <victorwccv@gmail.com>
2024-03-15 18:01:13 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,34 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
+
+
+app.post('/gemini', async (req, res) => {
+  console.log(req.body.history);
+  console.log(req.body.message);
+  const model = genAI.getGenerativeModel({model: 'gemini-pro'})
+  const chat = model.startChat({
+    history: req.body.history
+  })
+  const msg = req.body.message;
+
+  const result = await chat.sendMessage(msg);
+  const response  = await result.response;
+  const text = response.text();
+  res.send(text);
+  console.log(text);
+
+})
+
+    
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));     
